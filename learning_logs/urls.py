486bd892664b98eff881c1bf040a11e1752ba6d0
zaargham <48851583+zaargham@users.noreply.github.com>
2020-07-06 18:28:53 +0530

    Add files via upload

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     url(r'^topics/$', views.topics, name='topics'),
     # Detail page for a single topic
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    # Page for adding a new topic
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    # Page for adding a new entry
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    # Page for editing entries
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry,name='edit_entry'),
 ]
